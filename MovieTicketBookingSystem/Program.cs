93c7b3bed68823f3bcdfb5910ae4f624266da1b7
abilashini0712 <poovathyabilashini203@gmail.com>
2026-09-21 16:13:53 +0530

    connect register page into database

--- MovieTicketBookingSystem/Program.cs
+++ MovieTicketBookingSystem/Program.cs
@@ -3,27 +3,31 @@ using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add Razor Components
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add MudBlazor
 builder.Services.AddMudServices();
+
+// Add HttpClient
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
